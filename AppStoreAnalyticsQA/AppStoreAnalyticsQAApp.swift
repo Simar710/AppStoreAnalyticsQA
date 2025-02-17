3df7869582245d80c99bcf9be6fa6c5a1810cef0
Simar710 <simardeepsingh710@gmail.com>
2025-02-16 19:25:03 -0500

    Initial Commit

---
+++ AppStoreAnalyticsQA/AppStoreAnalyticsQAApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppStoreAnalyticsQAApp.swift
+//  AppStoreAnalyticsQA
+//
+//  Created by Simardeep Singh on 2/16/25.
+//
+
+import SwiftUI
+
+@main
+struct AppStoreAnalyticsQAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AppStoreAnalyticsQATests/AppStoreAnalyticsQATests.swift
@@ -0,0 +1,17 @@
+//
+//  AppStoreAnalyticsQATests.swift
+//  AppStoreAnalyticsQATests
+//
+//  Created by Simardeep Singh on 2/16/25.
+//
+
+import Testing
+@testable import AppStoreAnalyticsQA
+
+struct AppStoreAnalyticsQATests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
